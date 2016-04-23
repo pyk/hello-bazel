2be9e4833125bb302c47e2452e4e9464b76d537c
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-04-23 03:21:29 +0000

    add println to ProjectRunner.java

--- src/main/java/com/example/ProjectRunner.java
+++ src/main/java/com/example/ProjectRunner.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class ProjectRunner {
     public static void main(String args[]) {
+        System.out.println("Hello this is a edit");
         Greeting.sayHi();
     }
 }
